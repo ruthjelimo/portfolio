650ca37122af8b0b2b6bed1a69cef73914dc855a
ruthjelimo <ruthjmimo@gmail.com>
2022-06-23 23:14:08 +0300

    added a component

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar
+ = () => {
+    return ( 
+        <nav className="navbar">
+            
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Contact</li>
+            
+        </nav>
+     );
+}
+ 
+export default Navbar
+;
