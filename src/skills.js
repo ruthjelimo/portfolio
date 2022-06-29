273759f6f8cba8d380f9950ec271aeb451b593e1
ruthjelimo <ruthjmimo@gmail.com>
2022-06-29 11:16:39 +0300

    uipdated skills

--- src/skills.js
+++ src/skills.js
@@ -2,7 +2,16 @@ const skills = () => {
     return (  
         <div className="skills">
            <p>These are the skills that I am comfortable working with</p> 
+        <li>React</li>
+        <li>Angular</li>
+        <li>Django</li>
+        <li>Flask</li>
+        <li>Python</li>
+        <li>HTML</li>
+        <li>Javascript</li>
+        <li>Postgress</li>
         </div>
+    
     );
 }
  
